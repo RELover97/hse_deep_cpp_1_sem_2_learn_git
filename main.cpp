af2078ec42432d1405be6398c21579574857c595
RELover97 <re.forever.lover.97@gmail.com>
2026-09-14 00:47:46 +0300

    stack v.s. heap

--- main.cpp
+++ main.cpp
@@ -1,4 +1,5 @@
 #include <iostream>
+#include <unistd.h> // getpid()
 
 struct Cell {
     int x;
@@ -8,5 +9,17 @@ struct Cell {
 int main() 
 {
     std::cout << "Battleship!\n";
+
+    Cell cell{1, 2}; // in stack
+
+    Cell* other = new Cell{3, 4}; // in heap
+
+    std::cout << &cell << '\n'; // address of cell
+
+    std::cout << "PID: "
+              << getpid() << '\n';  // process ID (= program ID in Linux)
+
+    std::cin.get(); // waiting for one character from command line input
+
     return 0;
 }
